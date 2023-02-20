d411da829c36da5dcbec3e4c94e68c41328ccc29
Krishn Mehta <krishnmehta11@gmail.com>
2023-02-20 18:32:00 +0530

    Assignment-6

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Enter the URL: "); //Input the URL
+        string url = Console.ReadLine();
+
+        if(string.IsNullOrEmpty(url)) //If URL is empty 
+        {
+            Console.WriteLine("You have not entered URL");
+        }
+        else
+        {
+            Console.WriteLine("Your content is downloading");
+            string content = await DownloadContentAsync(url); //Store content in String
+
+            string filename = "A.txt";
+            await WriteContentAsync(filename, content);
+            Console.WriteLine("Work Done");
+        }
+    }
+
+    static async Task<string> DownloadContentAsync(string url)
+    {
+        using (var client = new HttpClient())
+        {
+            HttpResponseMessage res = await client.GetAsync(url);
+            return await res.Content.ReadAsStringAsync();
+        }
+    }
+    static async Task WriteContentAsync(string filename, string content)
+    {
+        using (var writer = new StreamWriter(filename)) // Using StreamWriter to write in file
+        {
+            await writer.WriteAsync(content);
+        }
+    }
+}
